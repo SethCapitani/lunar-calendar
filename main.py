af21a435d611a480aa7f9213ba68ba78649ce4ef
Seth <58305002+SethCapitani@users.noreply.github.com>
2024-02-07 17:31:25 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Moon Phase Calendar
+Author: Seth Capitani
+Description: This program calculates and displays the moon phase for a user specified date(s).
+"""
+
+from mc_functions import *
+
+print("\n"
+      "                 🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘 🌑\n"
+      "====================| The Moon Calendar |====================\n"
+      "\n"
+      "                Welcome to the Moon Calendar!\n"
+      "    Here you can find any Moon Phase between 2024 - 2050.\n"
+      "-------------------------------------------------------------\n"
+      "")
+
+if __name__ == "__main__":
+    main()
+
+print("=============================================================\n"
+      "              Thank you for using Moon Calendar!\n"
+      "                     Author: Seth Capitani\n"
+      "")
